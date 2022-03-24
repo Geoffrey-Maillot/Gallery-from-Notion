6ef995d9b118352b8a00855eab2404b55a80910a
geoffrey <geoffrey.maillot@gmail.com>
2022-03-24 08:59:33 +0100

    Fix bug

--- README.md
+++ README.md
@@ -60,3 +60,6 @@ npm run
 [![portfolio](https://img.shields.io/badge/my_portfolio-000?style=for-the-badge&logo=ko-fi&logoColor=white)](https://maillot-geoffrey-portfolio.xyz/)
 
 [![linkedin](https://img.shields.io/badge/linkedin-0A66C2?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/geoffrey-maillot-06a1411bb/)
+
+
+// RAjouter les variables d'env
--- src/component/Gallery/Columns.js
+++ src/component/Gallery/Columns.js
@@ -1,34 +1,46 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-
 // import plugins
 import LazyLoad from 'react-lazyload';
 
 // Styles =>
 import './styles.module.scss';
 
-
 // => Component
 const Columns = ({ images, setModalIsOpen, setDisplayedImageModal }) => {
-
   const onClickOpenModal = (e) => {
-    setDisplayedImageModal(e.target.dataset.id)
-    setModalIsOpen(true)
-  }
-  const onKeyDownOpenModal = ({ keyCode }) => {
-    if (keyCode === 19) {
-      setModalIsOpen(true)
+    setDisplayedImageModal(e.target.dataset.id);
+    setModalIsOpen(true);
+  };
+  const onKeyDownOpenModal = (e) => {
+    console.log(e.target.dataset.id)
+    if (e.keyCode === 13) {
+      setDisplayedImageModal(e.target.dataset.id);
+      setModalIsOpen(true);
     }
-  }
+  };
 
   // Return component =>
   return (
-    <div className='column'>
+    <div className="column">
       {images.map((image) => (
         <LazyLoad height={'100%'} key={image.id}>
-          <figure className='item' role="dialog" tabIndex={0} onClick={(e) => onClickOpenModal(e)} onKeyDown={(e) => onKeyDownOpenModal(e)}>
-            <img src={image.properties?.Image?.files[0]?.external?.url} alt={image.properties?.Categorie?.multi_select[0]?.name} data-id={image.id} async lazy="true" />
+          <figure
+            className="item"
+            role="dialog"
+            tabIndex={0}
+            onClick={(e) => onClickOpenModal(e)}
+            onKeyDown={(e) => onKeyDownOpenModal(e)}
+            data-id={image.id}
+          >
+            <img
+              src={image.properties?.Image?.files[0]?.external?.url}
+              alt={image.properties?.Categorie?.multi_select[0]?.name}
+              data-id={image.id}
+              async
+              lazy="true"
+            />
           </figure>
         </LazyLoad>
       ))}
@@ -39,9 +51,7 @@ const Columns = ({ images, setModalIsOpen, setDisplayedImageModal }) => {
 Columns.propTypes = {
   images: PropTypes.array.isRequired,
   setModalIsOpen: PropTypes.func.isRequired,
-  setDisplayedImageModal: PropTypes.func.isRequired
+  setDisplayedImageModal: PropTypes.func.isRequired,
 };
-
-
 
 export default Columns;
